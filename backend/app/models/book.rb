b2dd7679dc007b32c358418818cb37e9a279cfed
stanw0ng <user.stanley@gmail.com>
2023-05-13 12:48:11 -0400

    create Book model

---
+++ backend/app/models/book.rb
@@ -0,0 +1,5 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  
+  validates :name, presence: true
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_13_162900) do
+ActiveRecord::Schema.define(version: 2023_05_13_164531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.integer "recipe_id"
