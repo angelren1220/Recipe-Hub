930842b5146f39964c8817f947f0faf68a4d9ae3
stanw0ng <user.stanley@gmail.com>
2023-05-13 14:42:27 -0400

    create Recipes controller

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding Data ..."
+
+# Helper functions
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
+end
+
+# Only run on development (local) instances not on production, etc.
+unless Rails.env.development?
+  puts "Development seeds only (for now)!"
+  exit 0
+end
+
+# Let's do this ...
+
+## USERS
+
+puts "Creating Users ..."
+
+## RECIPES
+
+puts "Creating Recipes ..."
+
+## INGREDIENTS
+
+puts "Creating Ingredients ..."
+
+## RECIPE_BOOK (entrys for a book)
+
+puts "Creating Recipe Book Entries ..."
+
+## BOOKS
+
+puts "Creating Books ..."
+
+## BOOKMARKED_BOOK
+
+puts "Creating Bookmarked Books ..."
+
+
+
+puts "DONE!"
