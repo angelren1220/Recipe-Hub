f311be8bbe5fcb3119763e800b05c49410a8869d
stanw0ng <user.stanley@gmail.com>
2023-05-13 14:58:29 -0400

    add column for image in Recipes table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_13_170856) do
+ActiveRecord::Schema.define(version: 2023_05_13_185425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2023_05_13_170856) do
     t.boolean "is_nutfree", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "image"
   end
 
   create_table "users", force: :cascade do |t|
