e25fe56634205de7d0b1fd18352801442bf6f086
Linuxe <angelren1220@gmail.com>
2023-05-14 03:55:31 -0400

    add display lists of users in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useApplicationData from './hooks/useApplicationData';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const {
+    state,
+    dispatch
+  } = useApplicationData();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
+  ));
 
-export default App
+  return (<div className="App" >
+    <h1> Users </h1>
+
+    <ul> {userList} </ul>
+  </div >
+  );
+
+};
+
+export default App;
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -4,34 +4,34 @@ import {
 } from 'react';
 import dataReducer, {
   SET_USERS
-} from 'dataReducer';
+} from './dataReducer';
 import axios from 'axios';
 
 const useApplicationData = () => {
   const [state, dispatch] = useReducer(dataReducer, {
-      users: [],
-      loading: true,
+    users: [],
+    loading: true,
   });
   useEffect(() => {
-      axios({
-              method: 'GET',
-              url: '/api/users',
-          })
-          .then(({
-              data
-          }) => {
-              console.log(data);
-              dispatch({
-                  type: SET_USERS,
-                  users: data
-              });
-          })
-          .catch((err) => console.log(err));
+    axios({
+      method: 'GET',
+      url: '/api/users',
+    })
+      .then(({
+        data
+      }) => {
+        console.log(data);
+        dispatch({
+          type: SET_USERS,
+          users: data
+        });
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return {
-      state,
-      dispatch,
+    state,
+    dispatch,
   };
 };
 
