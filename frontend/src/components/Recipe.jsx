6d128fcb6408b14da2e9ed29428dc97d1b8315c1
Linuxe <angelren1220@gmail.com>
2023-05-15 16:09:31 -0400

    add props to recipes

--- frontend/src/components/Recipe.jsx
+++ frontend/src/components/Recipe.jsx
@@ -9,9 +9,18 @@ const Recipe = function(props) {
 
       <div className="recipe-text">
         <h2 className="recipe-title">{props.name}</h2>
+        <ul>
+          <li>Estimate cooktime: {props.cooktime} min</li>
+          {props.is_vegetarian && <li>Vegetarian</li>}
+          {props.is_vegans_lowcarb && <li>Low Carb</li>} 
+          {props.is_lactosefree && <li>Lactose Free</li>}
+          {props.is_glutenfree && <li>Gluten Free</li>}
+          {props.is_nutfree && <li>Nut Free</li>}
+        </ul>
         <ol>
           {props.directions.map(direction => (<li> {direction} </li>))}
         </ol>
+
       </div>
 
       <img src={props.image} alt={props.name} className="recipe-img" />
