25bea683454ed326544dd2e17cde209b2ed84322
stanw0ng <user.stanley@gmail.com>
2023-05-17 18:54:19 -0400

    add additional routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 
 {/*Page Components*/}
-import MyRecipes from './components/pages/MyRecipes';
+
+import RecipesList from './components/pages/RecipesList';
+import Recipe from './components/pages/Recipe';
+import NewRecipe from './components/pages/NewRecipe';
+import EditRecipe from './components/pages/EditRecipe';
+
+import Books from './components/pages/Books';
+import NewBook from './components/pages/NewBook';
+
 import Explore from './components/pages/Explore';
 import Search from './components/pages/Search';
+
 import GroceryLists from './components/pages/GroceryLists';
+import GroceryList from './components/pages/GroceryList';
+import GroceryListForm from './components/pages/GroceryListForm';
+
 import Profile from './components/pages/Profile';
+
 import Inbox from './components/pages/Inbox';
+import Message from './components/pages/Message';
+import NewMessage from './components/pages/NewMessage';
 
-
+{/*Single Components*/}
 import Navigation from './components/Navigation';
 import RecipeList from './components/RecipeList';
 import Login from './components/Login';
@@ -39,34 +54,42 @@ const App = function() {
         <Navigation />
 
         <Routes>
-          <Route path='/' element={<MyRecipes />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/explore' element={<Explore />}/>
-          <Route path='/search' element={<Search />}/>
-          <Route path='/grocerylists' element={<GroceryLists />}/>
-          <Route path='/profile' element={<Profile />}/>
-          <Route path='/inbox' element={<Inbox />}/>
+
+          <Route path='/recipes'>
+            <Route index element={<RecipesList/>}/>
+            <Route path=':id' element={<Recipe/>}/>
+            <Route path='new' element={<NewRecipe/>}/>
+            <Route path='edit/:id' element={<EditRecipe/>}/>
+          </Route>
+
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/explore' element={<Explore/>}/>
+          <Route path='/search' element={<Search/>}/>
+
+          <Route path='/books'>
+            <Route index element={<Books/>}/>
+            <Route path=':id' element={<RecipesList/>}/>
+            <Route path='new' element={<NewBook/>}/>
+          </Route>
+
+          <Route path='/grocerylists'>
+            <Route index element={<GroceryLists/>}/>
+            <Route path=':id' element={<GroceryList/>}/>
+            <Route path='new' element={<GroceryListForm />}/>
+          </Route>
+          
+          <Route path='/profile'>
+            <Route index element={<Profile/>}/>
+            <Route path=':id' element={<Profile/>}/>
+          </Route>
+
+          <Route path='/inbox'>
+            <Route index element={<Inbox/>}/>
+          </Route>
+
         </Routes>
       
       </Router>
-
-      <main className="recipes">
-
-        {viewMode === 'recipes' &&
-          <RecipeList
-            recipes={state.recipes}
-          />}
-        {viewMode === 'login' && <Login />}
-        {viewMode === 'register' && <Register />}
-        {viewMode === 'loading' && <Loading >Loading</Loading>}
-      </main>
-      
-      <footer>
-        <button onClick={loginView}>Log In</button >
-        <button onClick={registerView}>Register</button >
-        <button onClick={recipesView}>Recipes</button >
-        <button onClick={loadingView}>Loading...</button >
-      </footer>
 
     </div>
   );
---
+++ frontend/src/components/pages/Recipe.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+const Recipe = function(props) {
+
+  return (
+    <article className="recipe">
+      <h1>Single recipe information goes here</h1>
+    </article>
+  );
+};
+
+export default Recipe;
