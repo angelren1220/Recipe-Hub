6b1cd040d17c142b9b5e6486a35ec8e38bba349e
stanw0ng <user.stanley@gmail.com>
2023-05-18 19:25:42 -0400

    create Template component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,10 +27,8 @@ import Profile from './components/pages/Profile';
 import Inbox from './components/pages/Inbox';
 
 {/*Single Components*/}
-import Navigation from './components/Navigation';
+import Template from './components/Template';
 import useApplicationData from './hooks/useApplicationData';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
 
 const App = function() {
 
@@ -49,46 +47,47 @@ const App = function() {
 
     <div className="App">
       <Router>
-        <Navigation />
+        <Template>
 
-        <Routes>
+          <Routes>
 
-          <Route path='/recipes'>
-            <Route index element={<RecipesList/>}/>
-            <Route path=':id' element={<Recipe/>}/>
-            <Route path='new' element={<NewRecipe/>}/>
-            <Route path='edit/:id' element={<EditRecipe/>}/>
-          </Route>
-          
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
+            <Route path='/recipes'>
+              <Route index element={<RecipesList/>}/>
+              <Route path=':id' element={<Recipe/>}/>
+              <Route path='new' element={<NewRecipe/>}/>
+              <Route path='edit/:id' element={<EditRecipe/>}/>
+            </Route>
+            
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
 
-          <Route path='/explore' element={<Explore/>}/>
-          <Route path='/search' element={<Search/>}/>
+            <Route path='/explore' element={<Explore/>}/>
+            <Route path='/search' element={<Search/>}/>
 
-          <Route path='/books'>
-            <Route index element={<Books/>}/>
-            <Route path=':id' element={<RecipesList/>}/>
-            <Route path='new' element={<NewBook/>}/>
-          </Route>
+            <Route path='/books'>
+              <Route index element={<Books/>}/>
+              <Route path=':id' element={<RecipesList/>}/>
+              <Route path='new' element={<NewBook/>}/>
+            </Route>
 
-          <Route path='/grocerylists'>
-            <Route index element={<GroceryLists/>}/>
-            <Route path=':id' element={<GroceryList/>}/>
-            <Route path='new' element={<GroceryListForm />}/>
-          </Route>
-          
-          <Route path='/profile'>
-            <Route index element={<Profile/>}/>
-            <Route path=':id' element={<Profile/>}/>
-          </Route>
+            <Route path='/grocerylists'>
+              <Route index element={<GroceryLists/>}/>
+              <Route path=':id' element={<GroceryList/>}/>
+              <Route path='new' element={<GroceryListForm />}/>
+            </Route>
+            
+            <Route path='/profile'>
+              <Route index element={<Profile/>}/>
+              <Route path=':id' element={<Profile/>}/>
+            </Route>
 
-          <Route path='/inbox'>
-            <Route index element={<Inbox/>}/>
-          </Route>
+            <Route path='/inbox'>
+              <Route index element={<Inbox/>}/>
+            </Route>
 
-        </Routes>
-      
+          </Routes>
+
+        </Template>
       </Router>
     </div>
   );
