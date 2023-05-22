38a7f7ad5638461ba64915bc234d05a4c323f3e4
Linuxe <angelren1220@gmail.com>
2023-05-22 16:44:03 -0400

    fix bug explore page only show user's recipes

--- frontend/src/components/pages/Explore.jsx
+++ frontend/src/components/pages/Explore.jsx
@@ -4,10 +4,21 @@ import RecipeAccordion from "../RecipeAccordion";
 
 const Explore = function(props) {
 
+  const {
+    state,
+    getAllRecipes,
+  } = useApplicationData();
+
+  useEffect(() => {
+
+      getAllRecipes();
+
+  }, []);
+
   return (
     <article className="explore">
       <h1>ALL Recipes go here</h1>
-      <RecipeAccordion />
+      <RecipeAccordion recipes={state.recipes}/>
     </article>
   );
 };
--- frontend/src/components/pages/RecipesList.jsx
+++ frontend/src/components/pages/RecipesList.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useEffect} from "react";
 import RecipeAccordion from "../RecipeAccordion";
 import Loop from "../LoopScroll";
+import useApplicationData from "../../hooks/useApplicationData";
 
 const RecipesList = function(props) {
+
+  const {
+    state,
+    getRecipesByUserId,
+    getAllRecipes,
+  } = useApplicationData();
+
+  const userId = localStorage.getItem('userId');
+  useEffect(() => {
+    if (userId){
+      getRecipesByUserId(userId);
+    } else {
+      getAllRecipes();
+    }
+  }, []);
 
   return (
     <article className="recipes-list">
       <h1>A template to show recipes here</h1>
-      <RecipeAccordion />
+      <RecipeAccordion recipes={state.recipes} userId={userId}/>
     </article>
   );
 };
